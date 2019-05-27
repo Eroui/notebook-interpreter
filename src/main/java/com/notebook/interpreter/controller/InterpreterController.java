fe3e769e73883d070103d6fa3c2a8dae13f976e7
Abdelaziz EROUI <kapi.abdel@gmail.com>
2019-05-27 19:46:35 +0100

    Create Request & Response Models

--- src/main/java/com/notebook/interpreter/controller/InterpreterController.java
+++ src/main/java/com/notebook/interpreter/controller/InterpreterController.java
@@ -1,13 +1,19 @@
 package com.notebook.interpreter.controller;
 
+import com.notebook.interpreter.model.InterpreterRequest;
+import com.notebook.interpreter.model.InterpreterResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InterpreterController {
 
     @PostMapping("/execute")
-    public String execute() {
-        return "";
+    public ResponseEntity<InterpreterResponse> execute(@RequestBody InterpreterRequest interpreterRequest) {
+        InterpreterResponse interpreterResponse = new InterpreterResponse();
+        interpreterResponse.setResponse(interpreterRequest.getCode());
+        return ResponseEntity.ok(interpreterResponse);
     }
 }
---
+++ src/main/java/com/notebook/interpreter/model/InterpreterResponse.java
@@ -0,0 +1,13 @@
+package com.notebook.interpreter.model;
+
+public class InterpreterResponse {
+    private String response;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
