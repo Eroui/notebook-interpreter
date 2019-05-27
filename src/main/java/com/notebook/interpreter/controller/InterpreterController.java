ee5c1815c912f0b44bf2d46e41c96daed674a193
Abdelaziz EROUI <kapi.abdel@gmail.com>
2019-05-27 19:57:01 +0100

    Add Interpreter Request Validation

--- src/main/java/com/notebook/interpreter/controller/InterpreterController.java
+++ src/main/java/com/notebook/interpreter/controller/InterpreterController.java
@@ -2,16 +2,19 @@ package com.notebook.interpreter.controller;
 
 import com.notebook.interpreter.model.InterpreterRequest;
 import com.notebook.interpreter.model.InterpreterResponse;
+import com.notebook.interpreter.validation.CorrectRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class InterpreterController {
 
     @PostMapping("/execute")
-    public ResponseEntity<InterpreterResponse> execute(@RequestBody InterpreterRequest interpreterRequest) {
+    public ResponseEntity<InterpreterResponse> execute(@CorrectRequest @RequestBody InterpreterRequest interpreterRequest) {
         InterpreterResponse interpreterResponse = new InterpreterResponse();
         interpreterResponse.setResponse(interpreterRequest.getCode());
         return ResponseEntity.ok(interpreterResponse);
