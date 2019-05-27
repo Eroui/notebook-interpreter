278dded33a0653c8acb816b2440f43a6da263e5a
Abdelaziz EROUI <kapi.abdel@gmail.com>
2019-05-27 23:16:02 +0100

    Add Implement InterpreterService to PythonInterpreterService

--- src/main/java/com/notebook/interpreter/service/impl/PythonInterpreterServiceImpl.java
+++ src/main/java/com/notebook/interpreter/service/impl/PythonInterpreterServiceImpl.java
@@ -1,7 +1,9 @@
 package com.notebook.interpreter.service.impl;
 
+import com.notebook.interpreter.service.InterpreterService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PythonInterpreterServiceImpl {
+public class PythonInterpreterServiceImpl implements InterpreterService {
+    
 }
